da083a0b1ad75bad4e87c4e2fe4834bf7d49215b
vaibhav-if <vaibhavagr711@gmail.com>
2021-11-27 13:52:37 +0530

    Add route nesting between task and sub-tasks

--- app/models/sub_task.rb
+++ app/models/sub_task.rb
@@ -1,3 +1,5 @@
 class SubTask < ApplicationRecord
     belongs_to :task
+    validates :title, presence: true
+    validates :status, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :phases
-  resources :attachments
-  resources :comments
-  resources :sub_tasks
-  # resources :tasks
+  # resources :phases
+  # resources :attachments
+  # resources :comments
   devise_for :users
   resources :projects do
     resources :tasks
+  end
+  resources :tasks do
+    resources :sub_tasks
   end
   root 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
